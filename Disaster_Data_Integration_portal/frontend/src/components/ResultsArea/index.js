c47dc177e02a1588a52bcd1776f982950939c8af
mazimweal <allanmazimwe@yahoo.com>
2021-07-19 13:36:50 +0300

    Remove polygon from table

--- Disaster_Data_Integration_portal/frontend/src/components/ResultsArea/index.js
+++ Disaster_Data_Integration_portal/frontend/src/components/ResultsArea/index.js
@@ -12,7 +12,9 @@ const ResultsArea = ({ results, error }) => {
                 <thead>
                   <tr>
                     <th className="IndexColumn" />
-                    {Object.keys(results[0]).map((keyName, index) => (
+                    {Object.keys(results[0]).filter((item) =>
+                      !item.toString().toLowerCase().includes("polygon")
+                    ).map((keyName, index) => (
                       <th key={index}>{keyName}</th>
                     ))}
                   </tr>
@@ -21,16 +23,18 @@ const ResultsArea = ({ results, error }) => {
                   {results.map((result) => (
                     <tr>
                       <td className="IndexColumn"><div>{(results.indexOf(result) + 1).toString()}</div></td>
-                      {Object.keys(result).map((keyName, index) => (
+                      {Object.keys(result).filter((item) =>
+                        !item.toString().toLowerCase().includes("polygon")
+                      ).map((keyName, index) => (
                         <>
                           {Object.keys(result[keyName]).map((keyName2, index) => {
                             if (keyName2 === 'value') {
                               let valueString = result[keyName][keyName2].toString();
-                              
+
                               if (valueString.includes('http')) {
                                 valueString = valueString.split('#').pop();
                               }
-                              
+
                               return <td><div>{valueString}</div></td>
                             }
                             return null;
